bf1961bd89a9a18de7a26962b86f4a816007e5fa
jospin <lucien.carbonare@gmail.com>
2016-01-23 15:30:00 -0200

    Push do projeto de estudo

--- AluraC/AluraC/Form1.cs
+++ AluraC/AluraC/Form1.cs
@@ -16,5 +16,22 @@ namespace AluraC
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idadeJoao = 10;
+            int idadeMaria = 25;
+            double avg;
+            avg = (idadeJoao + idadeMaria) / 2;
+            MessageBox.Show("A média de idade é: " + avg);
+            int a = 6;
+            int b = 2;
+            int c = 2;
+            double delta ;
+            delta = b * b - 4 * a * c;
+            double a1 = -b + Math.Sqrt(delta) / (2 * a);
+            double a2 = -b - Math.Sqrt(delta) / (2 * a);
+            MessageBox.Show("Resultado de Bascha é a1 = " + a1 + " Ou a2 = " + a2);
+        }
     }
 }
